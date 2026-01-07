1bd8a4846dfa3de5e028edcd148f94eb47257c7b
SudeepSigdel <sudeepsigdel123@gmail.com>
2026-01-07 14:07:48 +0545

    Almost half done

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 window.addEventListener("load", displayName)
 
-function displayName(){
+function displayName() {
     const content = "hi, i'm sudeep."
     const length = content.length
     const span = document.getElementById("name")
     let i = 0;
-    const interval = setInterval(()=>{
-    if(i<length){
-        span.textContent += `${content[i]}`
-        i++;}
-    else{
-        clearInterval(interval);
-    }
-    }, 100)
+    const interval = setInterval(() => {
+        if (i < length) {
+            span.textContent += `${content[i]}`
+            i++;
+        }
+        else {
+            clearInterval(interval);
+        }
+    }, 80)
     return
 };
